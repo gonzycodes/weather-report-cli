016943d8e15a2c8066061402d5bc17529f760bfc
gonzycodes <nahuel.gonzalezj@hotmail.com>
2026-01-05 11:41:06 +0100

    finished the first function inside the storage module

--- src/weather/storage.py
+++ src/weather/storage.py
@@ -1,1 +1,9 @@
+from pathlib import Path
+
+from .config import CACHE_DIR
 ##  Read and Write JSON, cache, history, (no api code)
+def get_cache_path(lat,lon):
+    rounded_lat = round(float(lat), 3)
+    rounded_lon = round(float(lon), 3)
+    file_name = (f'{rounded_lat}_{rounded_lon}.json')
+    return CACHE_DIR/file_name
